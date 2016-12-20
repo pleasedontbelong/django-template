f683a9cf93ca2dceee1a27ac5fd159ac0a49d436
Pablo Leano <pleasedontbelong@gmail.com>
2016-12-20 16:06:59 +0100

    Fix template configuration

--- settings/addons/jinja2.py
+++ settings/addons/jinja2.py
@@ -1,16 +1,14 @@
-TEMPLATES = [
+# Full configuration in http://niwinz.github.io/django-jinja/latest/#_installation
+INSTALLED_APPS += ('django_jinja',)  # NOQA
+
+TEMPLATES += [  # NOQA
     {
         "BACKEND": "django_jinja.backend.Jinja2",
         "APP_DIRS": True,
         "OPTIONS": {
             "match_extension": ".jinja2",
             "app_dirname": "jinja2",
-            "auto_reload": DEBUG,
+            "auto_reload": DEBUG,  # NOQA
         }
-    },
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True
     },
 ]
