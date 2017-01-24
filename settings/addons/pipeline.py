02312230769d2a5602fbeb1b9ea44ced418f3aca
Pablo Leano <pleasedontbelong@gmail.com>
2017-01-24 13:54:29 +0100

    Fix comments for config with jinja2

--- settings/addons/pipeline.py
+++ settings/addons/pipeline.py
@@ -1,33 +1,34 @@
 # full configuration https://django-pipeline.readthedocs.io/en/latest/configuration.html
 
-INSTALLED_APPS += [
-    'pipeline',
-]
+INSTALLED_APPS += ("pipeline",)  # NOQA
 
-STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
+STATICFILES_STORAGE = "pipeline.storage.PipelineCachedStorage"
 
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'pipeline.finders.PipelineFinder',
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    "pipeline.finders.PipelineFinder",
 )
 
 # To enable with jinja2 add
-# 'OPTIONS': {
-#     'environment': 'myproject.jinja2.environment',
-#     'extensions': ['pipeline.jinja2.PipelineExtension']
+#
+# from django_jinja.builtins import DEFAULT_EXTENSIONS
+#
+# "OPTIONS": {
+#     "environment": "myproject.jinja2.environment",  # (optional) if you have one
+#     "extensions": DEFAULT_EXTENSIONS + ["pipeline.jinja2.PipelineExtension"]
 # }
-# to the jinja2 env
+# to the jinja2 settings
 
 
 PIPELINE = {
-    'PIPELINE_ENABLED': True,
-    'JAVASCRIPT': {
-        'main': {
-            'source_filenames': (
-                'js/jquery.js',
+    "PIPELINE_ENABLED": True,
+    "JAVASCRIPT": {
+        "main": {
+            "source_filenames": (
+                "js/jquery.js",
             ),
-            'output_filename': 'js/script.js',
+            "output_filename": "js/script.js",
         }
     }
 }
