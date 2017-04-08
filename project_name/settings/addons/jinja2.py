d411e311d89e641091d842155eaffdbe5ca2cdde
Pablo Leano <pleasedontbelong@gmail.com>
2017-04-08 19:13:11 +0200

    Add pipeline extension to jinja2 config

--- project_name/settings/addons/jinja2.py
+++ project_name/settings/addons/jinja2.py
@@ -1,4 +1,6 @@
 # Full configuration in http://niwinz.github.io/django-jinja/latest/#_installation
+from django_jinja.builtins import DEFAULT_EXTENSIONS
+
 INSTALLED_APPS += ('django_jinja',)  # NOQA
 
 TEMPLATES += [  # NOQA
@@ -9,6 +11,8 @@ TEMPLATES += [  # NOQA
             "match_extension": ".jinja2",
             "app_dirname": "jinja2",
             "auto_reload": True,  # Set to False in prod
+            "extensions": DEFAULT_EXTENSIONS + ["pipeline.jinja2.PipelineExtension"],
+            # "environment": "myproject.jinja2.environment",  # (optional) if you have one
         }
     },
 ]
