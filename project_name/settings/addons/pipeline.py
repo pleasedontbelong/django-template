48aaedf78a2fcab1cd50346ec1901baefd95578c
Pablo Leano <pleasedontbelong@gmail.com>
2017-04-08 19:11:33 +0200

    Remove comments on jinja2

--- project_name/settings/addons/pipeline.py
+++ project_name/settings/addons/pipeline.py
@@ -10,17 +10,6 @@ STATICFILES_FINDERS = (
     "pipeline.finders.PipelineFinder",
 )
 
-# To enable with jinja2 add
-#
-# from django_jinja.builtins import DEFAULT_EXTENSIONS
-#
-# "OPTIONS": {
-#     "environment": "myproject.jinja2.environment",  # (optional) if you have one
-#     "extensions": DEFAULT_EXTENSIONS + ["pipeline.jinja2.PipelineExtension"]
-# }
-# to the jinja2 settings
-
-
 PIPELINE = {
     "PIPELINE_ENABLED": True,
     "JAVASCRIPT": {
